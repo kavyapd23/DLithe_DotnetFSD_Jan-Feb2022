7426a02858e0d7e09f86edc95d60e7505a4872d8
kavya <kavyadaivagnachary@gmail.com>
2022-02-27 10:54:20 +0530

    commit

---
+++ Kavya_P_D/Front_End/c#/Task3/1-2dimensional.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace array_example2
+{
+	public class Program
+	{
+		public static void Main()
+		{//single dimensional array
+			int[] evenNums = new int[5];
+			evenNums[0] = 2;
+			evenNums[1] = 4;
+			evenNums[2] = 6;
+			evenNums[3] = 8;
+			evenNums[4] = 10;
+
+
+			Console.WriteLine(evenNums[0]);
+			Console.WriteLine(evenNums[1]);
+			Console.WriteLine(evenNums[2]);
+			Console.WriteLine(evenNums[3]);
+			Console.WriteLine(evenNums[4]);
+
+			//two dimensional Array
+			int[,] arr2d = new int[3, 2]{
+								{1, 2},
+								{3, 4},
+								{5, 6}
+							};
+
+			Console.WriteLine(arr2d[0, 0]);
+			Console.WriteLine(arr2d[0, 1]);
+			Console.WriteLine(arr2d[1, 0]);
+			Console.WriteLine(arr2d[1, 1]);
+			Console.WriteLine(arr2d[2, 0]);
+			Console.WriteLine(arr2d[2, 1]);
+
+		}
+	}
+}
