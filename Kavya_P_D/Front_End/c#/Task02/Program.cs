d86eb426cb03b1ea14c429170ffbd2df28d6c94b
kavya <kavyadaivagnachary@gmail.com>
2022-02-27 10:53:17 +0530

    commit

---
+++ Kavya_P_D/Front_End/c#/Task02/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    class Person 
+    {
+        private string name; 
+        string email; 
+
+
+        public string Name  
+        {
+            
+            get { return name; }
+            set { name = value; }
+
+        }
+
+        public Person() //Constructor (no return type)
+        {
+            email = "Romita@gmail.com";
+            Console.WriteLine(email);
+        }
+        public Person(out int a)
+        {
+            a = 10;
+
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person myObj = new Person();
+            Person my2 = new Person(out int x);
+            myObj.Name = "Romita";
+
+            Console.WriteLine(myObj.Name);
+            Console.Write(x);
+        }
+    }
+}
---
+++ Kavya_P_D/Front_End/c#/Task02/inheritance.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Inheritance //Single,multi-level,Hierarchical inheritance
+{
+    class Parent  // base class (parent) ,sealed class cannot be inherited
+    {
+        public string empname = "Lanjenbi";
+        public void feature()
+        {
+            Console.WriteLine("Parent class:Lanjenbi");
+        }
+    }
+
+    class Child : Parent // derived class (child)
+    {
+        public string location = "Manipur";
+    }
+
+    class Child1 : Parent //Hierarchical inheritance
+    {
+        public string salary = "55000";
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a ob object
+            Child ob = new Child();
+            Child1 ob1 = new Child1();
+
+            // Call the feature() method (From the Parent class) on the ob object
+            ob.feature();
+            ob1.feature();
+
+            Console.WriteLine(ob.empname + " " + ob.location);
+            Console.WriteLine(ob1.empname + " " + ob1.salary);
+        }
+    }
+}
