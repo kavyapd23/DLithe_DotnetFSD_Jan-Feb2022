ae7a9bf240fd7cc9efdd3d136ba1cdc67ce92a7c
kavya <kavyadaivagnachary@gmail.com>
2022-02-27 10:58:38 +0530

    commit

---
+++ Kavya_P_D/Front_End/c#/partial_class/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CompleteInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Record myRecord = new Record("Kavya", 50000, "Banglore");
+            myRecord.PrintRecord();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Kavya_P_D/Front_End/c#/partial_class/main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompleteInfo
+{
+    class info
+    {
+    }
+    public partial class Record
+    {
+        public void PrintRecord()
+        {
+            Console.WriteLine("Empname" + n);
+            Console.WriteLine("Salary:" + a);
+            Console.WriteLine("Location:" + b);
+        }
+    }
+}
