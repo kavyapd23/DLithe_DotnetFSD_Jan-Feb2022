67b45d36765c940c5bd072193edf5507e7b95b43
kavya <kavyadaivagnachary@gmail.com>
2022-02-27 11:02:14 +0530

    commit

---
+++ Kavya_P_D/Front_End/c#/Task5/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        int SNO, MARK1, MARK2, MARK3, AVERAGE;
+        string SNAME;
+        Console.WriteLine("&quot;ENTER THE STUDENT DETAILS&quot;");
+        Console.WriteLine("&quot; ENTER THE STUDENT NO&quot");
+        SNO = int.Parse("Console.ReadLine()");
+        Console.WriteLine("&quot; ENTER THE STUDENT NAME&quot;");
+        SNAME = ("Console.ReadLine");
+        Console.WriteLine("&quot; ENTER THE MARKS OF 3 SUBJECTS & quot;");
+        MARK1 = int.Parse("Console.ReadLine()");
+        MARK2 = int.Parse("Console.ReadLine()");
+        MARK3 = int.Parse("Console.ReadLine()");
+        int   TOTAL = MARK1 + MARK2 + MARK3;
+        AVERAGE = TOTAL;
+        Console.WriteLine("&quot;\n\n\nTHE STUDENT DETAILS ARE GIVEN BELOW :&quot;");
+Console.WriteLine("&quot; THE STUDENT NO IS: &quot; SNO");
+        Console.WriteLine("&quot; THE STUUDENT NAME IS: &quot; +SNAME");
+        Console.WriteLine("&quot; TOTAL MARKS IS: &quot; +TOTAL");
+        Console.WriteLine("&quot; AVEARGE MAARK IS: &quot; +AVERAGE");
+        Console.ReadKey();
+    }
+}
---
+++ Kavya_P_D/Front_End/c#/Task5/inheritance.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace DLithe
+{
+    public class A
+    {
+        public string Name = "anil";
+        public void GetName()
+        {
+            Console.WriteLine("Name:, Name);
+       }
+}
+public B extends A
+{
+
+   public string Location;
+public void GetLocation()
+{
+    Console.WriteLine(&quot; Location: &quot;, Location);
+       }
+    }
+    public class C extends B
+{
+
+   public int Age;
+public void GetAge()
+{
+    Console.WriteLine(&quot; Age: { 0}
+    &quot;, Age);
+       }
+    }
+    class Program
+{
+    static void Main(string[] args)
+    {
+        C c = new C();
+        c.Name = &quot; John Doe&quot; ;
+        c.Location = &quot; Hyderabad & quot; ;
+        c.Age = 32;
+        Console.WriteLine(&quot;\nPress Any Key to Exit..&quot;);
+        Console.ReadLine();
+    }
+}
+}
